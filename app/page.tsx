e0e781dc5561046dba0123ad81599e004494bbd1
bsgcasa <somebod036@gmail.com>
2023-12-09 23:17:51 +0100

    first commit merge test1

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,12 @@
 import Image from 'next/image'
-import Herosection from './components/Herosection'
-import Navbar from './components/Navbar'
+
 
 
 export default function Home() {
   return (
     <main className="flex min-h-screen bg-black flex-col w-full items-center justify-between overflow-hidden ">
-      <section className='px-[100px] flex flex-col w-full items-center justify-between'>
-        <Navbar className='z-20 ' />
-        <Herosection />
-      </section>
      
+     merge test
       
      
     </main>
