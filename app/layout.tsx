6c5ae104ff22cbcfed15d92543b5fa959adf9bd2
bsgcasa <somebod036@gmail.com>
2023-12-20 21:35:10 +0100

    google authintication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-
+import Authprovider from '@/components/providers/Authprovider'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -16,7 +16,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+       <Authprovider>
+         {children}
+
+       </Authprovider>
+
+      </body>
     </html>
   )
 }
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,8 +1,9 @@
+import { signIn } from 'next-auth/react'
 import React from 'react'
 
 const Button = () => {
   return (
-  <button className='bg-red-700 z-20 hover:bg-gray-900  sm:block hidden w-[200px] group relative rounded-md font-medium my-6  py-3 text-white'>
+  <button onClick={()=>signIn("google")} className='bg-red-700 z-20 hover:bg-gray-900  sm:block hidden w-[200px] group relative rounded-md font-medium my-6  py-3 text-white'>
     <span className="ease absolute left-0 top-0 h-0 w-0 border-t-2 border-white transition-all duration-200 group-hover:w-full"></span>
     <span className="ease absolute right-0 top-0 h-0 w-0 border-r-2 border-white transition-all duration-200 group-hover:h-full"></span>
     <span className="ease absolute bottom-0 right-0 h-0 w-0 border-b-2 border-white transition-all duration-200 group-hover:w-full"></span>
